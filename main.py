ba90e5b62d680daf5e726663fc1e93cf03f89ae4
2020-5-01-abr <cool.5horonov@gmail.com>
2021-12-03 17:44:51 +0300

    Создан класс приложения и его конструктор

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 import sys  # sys нужен для передачи argv в QApplication
 from PyQt5 import QtWidgets
 import design  # Это наш конвертированный файл дизайна
+
+class App(QtWidgets.QMainWindow, design.Ui_MainWindow):
+    def __init__(self):
+        # Это здесь нужно для доступа к переменным, методам
+        # и т.д. в файле design.py
+        super().__init__()
+        self.setupUi(self)  # Это нужно для инициализации нашего дизайна
