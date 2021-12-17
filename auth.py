7d68253f79be252bb91ef9666ee30637381ea88d
2020-5-01-abr <cool.5horonov@gmail.com>
2021-12-18 00:50:35 +0300

    Исправлена структура класса AuthGui

--- auth.py
+++ auth.py
@@ -1,19 +1,41 @@
-import sys
+import sys, os
 from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QMainWindow, QApplication, QGridLayout, QWidget
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QIcon
 
+ICON_PATH = 'icons'
 
-class AuthGui(object):
+class AuthGui(QMainWindow):
     """Класс используется для работы с окном авторизации пользователей"""
 
-    def setupUi(self, MainWindow):
+    def __init__(self):
+        """Констрктор класса AuthGui"""
 
-        MainWindow.setObjectName("MainWindow")
-        MainWindow.setFixedSize(500, 300)
-        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        super().__init__()
+
+        self.setupUi()
+
+    def setupUi(self):
+
+        self.setFixedSize(500, 300)
+        self.setWindowTitle("Фоторедактор")
+        self.setWindowIcon(QIcon(os.path.join(ICON_PATH, "photoshop.png")))
+
+        # Отрисовка всех компонентов окна
+        self.createCentralWidget()
+
+        # Показ окна
+        self.show()
+
+    def createCentralWidget(self):
+        self.centralwidget = QtWidgets.QWidget()
         self.centralwidget.setObjectName("centralwidget")
 
         self.auth_button = QtWidgets.QPushButton(self.centralwidget)
+        self.auth_button.setText('Войти')
         self.auth_button.setGeometry(QtCore.QRect(190, 210, 131, 51))
+        self.auth_button.clicked.connect(self.auth)
         font = QtGui.QFont()
         font.setPointSize(16)
         self.auth_button.setFont(font)
@@ -41,6 +63,7 @@ class AuthGui(object):
         self.password_input.setObjectName("password_input")
 
         self.password_label = QtWidgets.QLabel(self.centralwidget)
+        self.password_label.setText('Пароль')
         self.password_label.setGeometry(QtCore.QRect(200, 110, 101, 31))
         font = QtGui.QFont()
         font.setPointSize(16)
@@ -50,7 +73,9 @@ class AuthGui(object):
         self.password_label.setAlignment(QtCore.Qt.AlignCenter)
         self.password_label.setObjectName("password_label")
 
-        self.login_label = QtWidgets.QLabel(self.centralwidget)
+        self.login_label = QtWidgets.QLabel()
+        self.login_label.setText('Логин')
+        self.login_label.setGeometry(QtCore.QRect(210, 40, 81, 21))
         font = QtGui.QFont()
         font.setPointSize(16)
         font.setBold(True)
@@ -58,24 +83,27 @@ class AuthGui(object):
         self.login_label.setFont(font)
         self.login_label.setAlignment(QtCore.Qt.AlignCenter)
         self.login_label.setObjectName("login_label")
-        self.login_label.setGeometry(QtCore.QRect(210, 40, 81, 21))
+        
 
-        MainWindow.setCentralWidget(self.centralwidget)
-        self.menu_bar = QtWidgets.QMenuBar(MainWindow)
+        editing_grid = QGridLayout()
+        editing_grid.addWidget(self.login_label, 1, 0, 1, 0)
+        editing_grid.addWidget(self.login_input, 2, 0, 1, 0)
+        editing_grid.addWidget(self.password_label, 3, 0, 1, 0)
+        editing_grid.addWidget(self.password_input, 4, 0, 1, 0)
+        editing_grid.addWidget(self.auth_button, 5, 0, 1, 0)
+
+        editing_grid.setRowStretch(6, 7)
+
+        # Инициализация виджета, используя сетку
+        container = QWidget()
+        container.setLayout(editing_grid)
+
+        self.menu_bar = QtWidgets.QMenuBar(self.centralwidget)
         self.menu_bar.setGeometry(QtCore.QRect(0, 0, 513, 21))
         self.menu_bar.setObjectName("menu_bar")
-        MainWindow.setMenuBar(self.menu_bar)
+        self.setMenuBar(self.menu_bar)
 
-        self.retranslateUi(MainWindow)
-        QtCore.QMetaObject.connectSlotsByName(MainWindow)
-
-    def retranslateUi(self, MainWindow):
-        _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "Вход в приложение"))
-        self.auth_button.setText(_translate("MainWindow", "Войти"))
-        self.password_label.setText(_translate("MainWindow", "Пароль"))
-        self.login_label.setText(_translate("MainWindow", "Логин"))
-
+        self.setCentralWidget(container)
 
     def start(self):
         app = QtWidgets.QApplication(sys.argv)
@@ -83,6 +111,19 @@ class AuthGui(object):
         ui = AuthGui()
         ui.setupUi(MainWindow)
         MainWindow.show()
-        sys.exit(app.exec_())
 
-AuthGui().start()
+    def auth(self):
+        username = self.login_input.text()
+        password = self.password_input.text()
+        print(username, password, sep=" ")
+        self.close()
+
+if __name__ == "__main__":
+
+    # Создание приложения QT
+    app = QApplication(sys.argv)
+    app.setAttribute(Qt.AA_DontShowIconsInMenus, True)
+
+    # Инициализация окна фоторедактора и его отображение
+    window = AuthGui()
+    sys.exit(app.exec_())
