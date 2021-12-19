b846518daa7497acc04400ea18ad2f5d98414a7c
2020-5-24-sho <cool.shoronov19@gmail.com>
2021-12-19 22:53:02 +0300

    Все GUI классы перенесены в один файл

--- gui.py
+++ gui.py
@@ -1,4 +1,4 @@
-from PyQt5 import QtWidgets
+from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import (QToolBar, QAction, QSlider, QGridLayout,
                              QWidget, QMainWindow, QLabel, QMessageBox,
                              QScrollArea, QDockWidget, QToolButton)
@@ -7,7 +7,7 @@ from PyQt5.QtCore import Qt, QSize
 from image import ImageLabel
 import styles
 import os
-from selection import SelectionGui
+from database import DatabaseQuery
 
 # Путь к папке с иконками
 ICON_PATH = 'icons'
@@ -248,3 +248,230 @@ class PhotoEditorGUI(QMainWindow):
     def backToSelection(self):
         # Возврат к списку изображений
         pass
+    
+class AuthGui(QMainWindow):
+    """Класс используется для работы с окном авторизации пользователей"""
+
+    def __init__(self):
+        """Констрктор класса AuthGui"""
+
+        super().__init__()
+        self.setupUi()
+
+    def setupUi(self):
+        """Главный метод, создающий окно и рисующий все компоненты"""
+
+        self.setFixedSize(500, 300)
+        self.setWindowTitle("Фоторедактор")
+        self.setWindowIcon(QIcon(os.path.join(ICON_PATH, "photoshop.png")))
+
+        # Отрисовка всех компонентов окна
+        self.createCentralWidget()
+
+        # Показ окна
+        # self.show()
+
+    def createCentralWidget(self):
+        """Метод, инициализирующий все компоненты окна"""
+        self.centralwidget = QtWidgets.QWidget()
+        self.centralwidget.setObjectName("centralwidget")
+
+        # Кнопка "Войти"
+        self.auth_button = QtWidgets.QPushButton(self.centralwidget)
+        self.auth_button.setText('Войти')
+        self.auth_button.setGeometry(QtCore.QRect(190, 210, 131, 51))
+        self.auth_button.clicked.connect(self.auth)
+        font = QtGui.QFont()
+        font.setPointSize(16)
+        self.auth_button.setFont(font)
+        self.auth_button.setAutoFillBackground(False)
+        self.auth_button.setCheckable(False)
+        self.auth_button.setObjectName("auth_button")
+
+        # Поле ввода логина
+        self.login_input = QtWidgets.QLineEdit(self.centralwidget)
+        self.login_input.setGeometry(QtCore.QRect(140, 70, 221, 31))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        self.login_input.setFont(font)
+        self.login_input.setText("")
+        self.login_input.setAlignment(QtCore.Qt.AlignCenter)
+        self.login_input.setObjectName("login_input")
+
+        # Поле ввода пароля
+        self.password_input = QtWidgets.QLineEdit(self.centralwidget)
+        self.password_input.setGeometry(QtCore.QRect(140, 150, 221, 31))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        self.password_input.setFont(font)
+        self.password_input.setInputMethodHints(QtCore.Qt.ImhHiddenText)
+        self.password_input.setText("")
+        self.password_input.setAlignment(QtCore.Qt.AlignCenter)
+        self.password_input.setObjectName("password_input")
+
+        # Надпись "Пароль"
+        self.password_label = QtWidgets.QLabel(self.centralwidget)
+        self.password_label.setText('Пароль')
+        self.password_label.setGeometry(QtCore.QRect(200, 110, 101, 31))
+        font = QtGui.QFont()
+        font.setPointSize(16)
+        font.setBold(True)
+        font.setWeight(75)
+        self.password_label.setFont(font)
+        self.password_label.setAlignment(QtCore.Qt.AlignCenter)
+        self.password_label.setObjectName("password_label")
+
+        # Надпись "Логин"
+        self.login_label = QtWidgets.QLabel()
+        self.login_label.setText('Логин')
+        self.login_label.setGeometry(QtCore.QRect(210, 40, 81, 21))
+        font = QtGui.QFont()
+        font.setPointSize(16)
+        font.setBold(True)
+        font.setWeight(75)
+        self.login_label.setFont(font)
+        self.login_label.setAlignment(QtCore.Qt.AlignCenter)
+        self.login_label.setObjectName("login_label")
+        
+        # Grid widget (сетка)
+        editing_grid = QGridLayout()
+        editing_grid.addWidget(self.login_label, 1, 0, 1, 0)
+        editing_grid.addWidget(self.login_input, 2, 0, 1, 0)
+        editing_grid.addWidget(self.password_label, 3, 0, 1, 0)
+        editing_grid.addWidget(self.password_input, 4, 0, 1, 0)
+        editing_grid.addWidget(self.auth_button, 5, 0, 1, 0)
+
+        editing_grid.setRowStretch(6, 7)
+
+        # Инициализация виджета, используя сетку
+        container = QWidget()
+        container.setLayout(editing_grid)
+
+        self.setCentralWidget(container)
+    
+    def auth(self):
+        """Функция проверяет введенные данные и определяет дальнейшее действие"""
+        
+        username = self.login_input.text()
+        password = self.password_input.text()
+        
+        response = DatabaseQuery().getUserPassword(username)
+        
+        if response != False:
+            if (password == response):
+                print('SUCCESS AUTH')
+                images = DatabaseQuery().getUserImages(username)
+                self.close()
+                self.selection = SelectionGui(username, images)
+                self.selection.show()
+            else:
+                print('FAIL AUTH')
+                # QErrorMessage
+        else:
+            print(f"NEW USER: {username}")
+            DatabaseQuery().registrateUser(username, password)
+            # mkdir
+            self.close()
+            self.selection = SelectionGui(username)
+            self.selection.show()
+            
+class SelectionGui(QMainWindow):
+    """Класс используется для работы с окном выбора изображения"""
+
+    def __init__(self, username, images = list()):
+        """Констрктор класса SelectionGui"""
+
+        self.images   = images
+        self.username = username
+        super().__init__()
+        self.setupUi()
+
+    def setupUi(self):
+        """Главный метод, создающий окно и рисующий все компоненты"""
+
+        self.setFixedSize(540, 360)
+        self.setWindowTitle("Фоторедактор")
+        self.setWindowIcon(QIcon(os.path.join(ICON_PATH, "photoshop.png")))
+
+        # Отрисовка всех компонентов окна
+        self.createCentralWidget()
+
+        # Показ окна
+        # self.show()
+
+    def createCentralWidget(self):
+        """Функция, создающая все компоненты окна"""
+
+        # Центральный (главный) виджет окна
+        self.centralwidget = QtWidgets.QWidget()
+        self.centralwidget.setObjectName("centralwidget")
+        font = QtGui.QFont()
+        font.setFamily("Calibri")
+        font.setPointSize(12)
+        font.setBold(True)
+        font.setWeight(75)
+        self.centralwidget.setFont(font)
+
+        # Сетка с компонентами окна
+        self.gridLayoutWidget = QtWidgets.QWidget(self.centralwidget)
+        self.gridLayoutWidget.setGeometry(QtCore.QRect(30, 30, 471, 271))
+        self.gridLayoutWidget.setObjectName("gridLayoutWidget")
+        self.gridLayout = QtWidgets.QGridLayout(self.gridLayoutWidget)
+        self.gridLayout.setContentsMargins(0, 0, 0, 0)
+        self.gridLayout.setObjectName("gridLayout")
+
+        # Кнопка "Загрузить"
+        self.load_button = QtWidgets.QPushButton(self.gridLayoutWidget)
+        self.load_button.setObjectName("load_button")
+        self.load_button.setText("Загрузить")
+        self.load_button.clicked.connect(self.loadImage)
+        self.gridLayout.addWidget(self.load_button, 3, 0, 1, 1)
+
+        # Кнопка "Редактировать"
+        self.edit_button = QtWidgets.QPushButton(self.gridLayoutWidget)
+        self.edit_button.setObjectName("edit_button")
+        self.edit_button.setText("Редактировать")
+        self.edit_button.clicked.connect(self.editImage)
+        self.gridLayout.addWidget(self.edit_button, 1, 0, 1, 1)
+
+        # Кнопка "Выйти"
+        self.exit_button = QtWidgets.QPushButton(self.gridLayoutWidget)
+        self.exit_button.setObjectName("exit_button")
+        self.exit_button.setText("Выйти")
+        self.exit_button.clicked.connect(self.exit)
+        self.gridLayout.addWidget(self.exit_button, 4, 0, 1, 1)
+
+        # Элемент - список изображений
+        self.list = QtWidgets.QListWidget(self.gridLayoutWidget)
+        self.list.setObjectName("list")
+        
+        # Добавление фотографий в список для отображения на экране
+        for image in self.images:
+            item = QtWidgets.QListWidgetItem()
+            item.setText(image)
+            self.list.addItem(item)
+
+        self.gridLayout.addWidget(self.list, 0, 0, 1, 1)
+
+        # Установка главного виджета
+        self.setCentralWidget(self.centralwidget)
+
+    def editImage(self):
+        """Функция предназначена для открытия окна фоторедактора"""
+        # Здеь будет открываться окно редактирования
+        print('Редактирование')
+        pass
+
+    def loadImage(self):
+        """Функция предназначена для загрузки изображения в базу"""
+        # Диалоговое окно выбора изображения
+        # Запись в БД
+        print('Загрузка')
+        pass
+
+    def exit(self):
+        """Функция предназначена для выхода из сессии пользователя"""
+        self.close()
+        self.auth = AuthGui()
+        self.auth.show()
+        print('Выход')
