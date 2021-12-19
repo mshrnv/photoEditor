b6f3a283080dcbbcefe229052a9d7a03954d982d
2020-5-21-tak <dimmaa155@gmail.com>
2021-12-19 14:47:41 +0300

    Реализована отрисвока интерфейса классом SelectionGui

--- selection.py
+++ selection.py
@@ -6,19 +6,19 @@ from PyQt5.QtGui import QIcon
 
 ICON_PATH = 'icons'
 
-
 class SelectionGui(QMainWindow):
 
-    def __init__(self):
+    def __init__(self, images = list()):
         """Констрктор класса SelectionGui"""
 
+        self.images = images
         super().__init__()
         self.setupUi()
 
     def setupUi(self):
         """Главный метод, создающий окно и рисующий все компоненты"""
 
-        self.setFixedSize(500, 300)
+        self.setFixedSize(540, 360)
         self.setWindowTitle("Фоторедактор")
         self.setWindowIcon(QIcon(os.path.join(ICON_PATH, "photoshop.png")))
 
@@ -28,23 +28,27 @@ class SelectionGui(QMainWindow):
         # Показ окна
         self.show()
 
-    def createCentralWidget(self, MainWindow):
-        MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(540, 358)
-        font = QtGui.QFont()
-        font.setFamily("Calibri")
-        font.setPointSize(12)
-        font.setBold(True)
-        font.setWeight(75)
-        MainWindow.setFont(font)
-        self.centralwidget = QtWidgets.QWidget(MainWindow)
+    def createCentralWidget(self):
+
+        # font = QtGui.QFont()
+        # font.setFamily("Calibri")
+        # font.setPointSize(12)
+        # font.setBold(True)
+        # font.setWeight(75)
+        # MainWindow.setFont(font)
+
+        # Центральный (главный) виджет окна
+        self.centralwidget = QtWidgets.QWidget()
         self.centralwidget.setObjectName("centralwidget")
+
+        # Сетка с компонентами окна
         self.gridLayoutWidget = QtWidgets.QWidget(self.centralwidget)
         self.gridLayoutWidget.setGeometry(QtCore.QRect(30, 30, 471, 271))
         self.gridLayoutWidget.setObjectName("gridLayoutWidget")
         self.gridLayout = QtWidgets.QGridLayout(self.gridLayoutWidget)
         self.gridLayout.setContentsMargins(0, 0, 0, 0)
         self.gridLayout.setObjectName("gridLayout")
+
         self.load_button = QtWidgets.QPushButton(self.gridLayoutWidget)
         font = QtGui.QFont()
         font.setFamily("Calibri")
@@ -53,7 +57,9 @@ class SelectionGui(QMainWindow):
         font.setWeight(75)
         self.load_button.setFont(font)
         self.load_button.setObjectName("load_button")
+        self.load_button.setText("Загрузить")
         self.gridLayout.addWidget(self.load_button, 3, 0, 1, 1)
+
         self.edit_button = QtWidgets.QPushButton(self.gridLayoutWidget)
         font = QtGui.QFont()
         font.setFamily("Calibri")
@@ -63,7 +69,9 @@ class SelectionGui(QMainWindow):
         self.edit_button.setFont(font)
         self.edit_button.setStyleSheet("")
         self.edit_button.setObjectName("edit_button")
+        self.edit_button.setText("Редактировать")
         self.gridLayout.addWidget(self.edit_button, 1, 0, 1, 1)
+
         self.exit_button = QtWidgets.QPushButton(self.gridLayoutWidget)
         font = QtGui.QFont()
         font.setFamily("Calibri")
@@ -72,47 +80,25 @@ class SelectionGui(QMainWindow):
         font.setWeight(75)
         self.exit_button.setFont(font)
         self.exit_button.setObjectName("exit_button")
+        self.exit_button.setText("Выйти")
         self.gridLayout.addWidget(self.exit_button, 4, 0, 1, 1)
+
         self.list = QtWidgets.QListWidget(self.gridLayoutWidget)
         self.list.setObjectName("list")
-        item = QtWidgets.QListWidgetItem()
-        self.list.addItem(item)
-        item = QtWidgets.QListWidgetItem()
-        self.list.addItem(item)
-        item = QtWidgets.QListWidgetItem()
-        self.list.addItem(item)
+        
+        for image in self.images:
+            item = QtWidgets.QListWidgetItem()
+            item.setText(image)
+            self.list.addItem(item)
+
         self.gridLayout.addWidget(self.list, 0, 0, 1, 1)
-        MainWindow.setCentralWidget(self.centralwidget)
-        self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 540, 25))
-        self.menubar.setObjectName("menubar")
-        MainWindow.setMenuBar(self.menubar)
-
-        self.retranslateUi(MainWindow)
-        QtCore.QMetaObject.connectSlotsByName(MainWindow)
-
-    def retranslateUi(self, MainWindow):
-        _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.load_button.setText(_translate("MainWindow", "Загрузить"))
-        self.edit_button.setText(_translate("MainWindow", "Редактировать"))
-        self.exit_button.setText(_translate("MainWindow", "Выйти"))
-        __sortingEnabled = self.list.isSortingEnabled()
-        self.list.setSortingEnabled(False)
-        item = self.list.item(0)
-        item.setText(_translate("MainWindow", "1.jpeg"))
-        item = self.list.item(1)
-        item.setText(_translate("MainWindow", "2.jpeg"))
-        item = self.list.item(2)
-        item.setText(_translate("MainWindow", "3.png"))
-        self.list.setSortingEnabled(__sortingEnabled)
-
+        self.setCentralWidget(self.centralwidget)
 
 if __name__ == "__main__":
-    import sys
-    app = QtWidgets.QApplication(sys.argv)
-    MainWindow = QtWidgets.QMainWindow()
-    ui = Ui_MainWindow()
-    ui.setupUi(MainWindow)
-    MainWindow.show()
+    # Создание приложения QT
+    app = QApplication(sys.argv)
+    app.setAttribute(Qt.AA_DontShowIconsInMenus, True)
+
+    # Инициализация окна фоторедактора и его отображение
+    window = SelectionGui(['1.jpg', '2.png'])
     sys.exit(app.exec_())
