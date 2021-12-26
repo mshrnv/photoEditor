036c08348830c08365e69e6599c852e94db1c8af
2020-5-24-sho <cool.shoronov19@gmail.com>
2021-12-27 02:39:38 +0300

    Добавлены комментарии к тестам

--- tests/main_test.py
+++ tests/main_test.py
@@ -8,33 +8,42 @@ from random import randint
 
 class TestClass:
     
+    # Генерация случайного логина, пароля и изображения
     login    = randint(1000,9999)
     password = randint(1000,9999)
     image    = str(randint(1000,9999)) + '.jpg'
-        
+    
+    # Проверка инициализации объекта класса    
     def test_init(self):
         database.DatabaseQuery()
-        
+    
+    # Проверка подключения к базе    
     def test_connect(self):
         database.DatabaseQuery()._connect()
-        
+    
+    # Проверка отключения от базы    
     def test_close(self):
         obj = database.Database()
         obj._connect()
         obj._close()
-        
+    
+    # Попытка регистрации    
     def test_reg(self):
         database.DatabaseQuery().registrate_user(self.login, self.password)
-        
+    
+    # Попытка повторной регистрации (ловим ошибку)    
     def test_repeated_reg(self):
         with pytest.raises(sqlite3.IntegrityError):
             database.DatabaseQuery().registrate_user(self.login, self.password)
-        
+    
+    # Проверка успешной авторизации    
     def test_auth(self):
         assert str(self.password) == database.DatabaseQuery().get_user_password(self.login)
-        
+    
+    # Попытка добавления изображения    
     def test_image_add(self):
         database.DatabaseQuery().add_image(self.login, self.image)
-        
+    
+    # Проверка наличия добавленного изображения в базе    
     def test_image_list(self):
         assert True == (self.image in database.DatabaseQuery().get_user_images(self.login))
